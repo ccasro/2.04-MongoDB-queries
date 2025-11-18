5732a0f7bd0aa28a3cfe98af49e7213864cd1b47
ccasro <ccasro@gmail.com>
2025-11-18 13:16:09 +0100

    chore(init.sh): add authentication parameters to mongoimport

--- init-mongo/init.sh
+++ init-mongo/init.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+set -e
 # importing JSON to restaurants collection
-mongoimport --db restaurants --collection restaurants --file /docker-entrypoint-initdb.d/restaurants.json
+mongoimport \
+  --username "$MONGO_INITDB_ROOT_USERNAME" \
+  --password "$MONGO_INITDB_ROOT_PASSWORD" \
+  --authenticationDatabase admin \
+  --db restaurants \
+  --collection restaurants \
+  --file /docker-entrypoint-initdb.d/restaurants.json
